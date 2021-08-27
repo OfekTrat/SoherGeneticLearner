d3244ab6a6d67da9744ee2dcd363ffd18e082fce
Ofek <ofekikotrat@gmail.com>
2021-08-27 18:40:39 +0300

    Added create filename and extract stock name from filename

--- stock_data_collector.py
+++ stock_data_collector.py
@@ -6,8 +6,11 @@ from typing import Optional, List, Dict, Union
 
 
 class StockDataCollector:
+    FILE_EXT = "pkl"
+    DEFUALT_INTERVAL = "1d"
+
     @classmethod
-    def collect(cls, stock_symbols: Union[List[str], str], interval: str = "1d",
+    def collect(cls, stock_symbols: Union[List[str], str], interval: str = DEFUALT_INTERVAL,
                 from_time: Optional[datetime] = None, to_time: Optional[datetime] = None) -> Dict[str, pd.DataFrame]:
         stocks = cls.__get_stocks_list(stock_symbols)
 
@@ -34,18 +37,27 @@ class StockDataCollector:
             os.mkdir(data_folder)
         
         for stock_name, stock_data in collected_data.items():
-            path = os.path.join(data_folder, stock_name + ".pkl")
+            filename = cls.__create_filename(stock_name)
+            path = os.path.join(data_folder, filename)
             stock_data.to_pickle(path)
 
-    @staticmethod
-    def load_collection(path: str) -> Dict[str, pd.DataFrame]:
+    @classmethod
+    def load_collection(cls, path: str) -> Dict[str, pd.DataFrame]:
         data = dict()
         filenames = os.listdir(path)
 
         for filename in filenames:
-            stock_name = filename.split(".")[0]
+            stock_name = cls.__extract_stock_name_from_filename(filename)
             full_path = os.path.join(path, filename)
             stock_data = pd.read_pickle(full_path)
             data[stock_name] = stock_data
 
         return data
+
+    @classmethod
+    def __create_filename(cls, stock_name: str) -> str:
+        return stock_name + "." + cls.FILE_EXT
+
+    @classmethod
+    def __extract_stock_name_from_filename(cls, filename: str) -> str:
+        return filename.split(".")[0]
